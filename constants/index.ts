0840c1b0a29b5407234d114dcc04ecdb9e4d8bb3
chong-antler <chong.chen@antler.co>
2023-03-21 23:57:53 +0800

    infra: add static files and constants

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+import icons from "constants/icons";
+import images from "constants/images";
+import { COLORS, FONT, SHADOWS, SIZES } from "constants/theme";
+
+export { images, icons, COLORS, FONT, SIZES, SHADOWS };
