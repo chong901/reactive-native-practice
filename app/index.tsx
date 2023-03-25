da5673ecf78da60af45fc50df5c6d6bc0cb21de6
chong-antler <chong.chen@antler.co>
2023-03-25 11:29:01 +0800

    Home: home page template

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from "react-native";
+import { COLORS, SIZES } from "constants/theme";
+import { Stack } from "expo-router";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => <ScreenHeaderButton />,
+          headerRight: () => <ScreenHeaderButton />,
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <PopularJobs />
+          <NearbyJobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const ScreenHeaderButton = () => <Text>Screen header button</Text>;
+
+const Welcome = () => <Text>Welcome</Text>;
+
+const PopularJobs = () => <Text>Popular jobs</Text>;
+const NearbyJobs = () => <Text>Nearby jobs</Text>;
