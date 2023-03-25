9480ceb2662650ef797bc5f6c0ffd3f52d1b4139
chong-antler <chong.chen@antler.co>
2023-03-25 14:52:44 +0800

    infra: setup fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [isFontsLoaded] = useFonts({
+    DMBold: require("assets/fonts/DMSans-Bold.ttf"),
+    DMRegular: require("assets/fonts/DMSans-Regular.ttf"),
+    DMMedium: require("assets/fonts/DMSans-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (isFontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [isFontsLoaded]);
+
+  if (!isFontsLoaded) {
+    return null;
+  }
   return <Stack />;
 }
