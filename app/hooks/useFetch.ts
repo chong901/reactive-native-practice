e87d94308b11c823f18b40e54bf221969dcb2eec
chong-antler <chong.chen@antler.co>
2023-03-30 16:09:47 +0800

    hook: add useFetch hook

---
+++ app/hooks/useFetch.ts
@@ -0,0 +1,36 @@
+import { RAPID_API_KEY } from "@env";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const rapidApiKey = RAPID_API_KEY;
+export const useFetch = <T>(path: string, params: Record<string, unknown>) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${path}`,
+    headers: {
+      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params,
+  };
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.request<T>(options);
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, loading, error, refetch: fetchData };
+};
